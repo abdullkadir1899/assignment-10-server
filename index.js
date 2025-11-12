9e54519d1d67398c56a4e9e54ded484d51a8382c
abdullkadir1899 <mdabdulkadir1899@gmail.com>
2025-11-12 19:19:25 +0600

    user

--- index.js
+++ index.js
@@ -25,15 +25,43 @@ const client = new MongoClient(uri, {
 });
 
 
-app.get('/', (req, res) => {
-    res.send('Ai Model Inventory Manager Server is Running')
-})
+
 
 async function run() {
   try {
     await client.connect();
+    console.log("mongodb connected success")
+
+    const database = client.db('assignment-10-server');
+    const modelsCollection = database.collection('data');
+    const usersCollection = database.collection('users');
+    const purchasedCollection = database.collection('add-data');
+
+    // rot
+    app.get('/', (req, res) => {
+        res.send('Ai Model Inventory Manager Server is Running')
+    })
 
 
+    // user
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        const query = {email: user.email};
+        try{
+            const existingUser = await usersCollection.findOne(query);
+            if(existingUser){
+                return res.send({success: false, message: 'User already exists'})
+            }
+        }
+        catch(error){
+            res.status(500).send({success: false, message: 'Failed to save user', error: error.message})
+        }
+    })
+
+
+
+
+    
 
 
     await client.db("admin").command({ ping: 1 });
