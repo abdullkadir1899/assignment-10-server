3f0801480bfed8d5c3c2b157363faaca9e26cc7e
abdullkadir1899 <mdabdulkadir1899@gmail.com>
2025-11-12 19:07:41 +0600

    mongodbadd

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,19 +9,43 @@ const port = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
 
-//  URI 
 
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//         version: ServerApiVersion.v1,
-//         strict: true,
-//         deprecationErrors: true,
-//     }
-// });
+//assignment-10-server
+//BepkTv6K12KvaoI7
+
+const uri = "mongodb+srv://assignment-10-server:BepkTv6K12KvaoI7@cluster0.ziilask.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 
 app.get('/', (req, res) => {
     res.send('Ai Model Inventory Manager Server is Running')
 })
+
+async function run() {
+  try {
+    await client.connect();
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
+
+
 
 
 app.listen(port, () => {
